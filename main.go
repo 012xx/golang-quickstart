5bb748346db044a820cc5b968f5e7958c96d9dee
012xx <night.fox.045@gmail.com>
2024-01-31 16:20:19 +0900

    Add: comment

--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 )
 
 func main() {
-	router := gin.Default()
-	router.LoadHTMLGlob("template/*.html")
+	router := gin.Default()                // Ginルーターの新しいインスタンスを作成。HTTPリクエストを処理するためのルートやミドルウェアの設定を管理する。
+	router.LoadHTMLGlob("template/*.html") // HTMLを読み込むディレクトリを指定
 
+	// ルートURL（/）に対するGETリクエストを処理するためのハンドラーを設定。
+	// この関数は、リクエストが来た際に呼び出される。ここでは、ステータスコード200とともに index.html テンプレートをレンダリングする。
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.HTML(200, "index.html", gin.H{})
 	})
+	// サーバーを起動し、デフォルトのポート（8080）で待ち受けを開始する
 	router.Run()
 }
